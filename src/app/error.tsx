303a6386b96f66c310c39a37b164cf2afe335bb9
nicchy123 <nasirchy252@gmail.com>
2023-10-22 18:45:49 +0600

    initial project

---
+++ src/app/error.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            something went wrong
+        </div>
+    );
+};
+
+export default Error;
